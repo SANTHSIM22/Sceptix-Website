6dac7bb174036e244532bcd375b6ae9f538db5c9
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-09-06 22:22:23 +0530

    Feat: front blog page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+const page = () => {
+  return (
+    <> 
+    <div>
+        <div className='className="flex justify-center items-center w-full h-20"'>
+         <h1 className='md:text-5xl text-3xl font-extrabold mt-5 md:ml-5 justify-center text-center font-fira_sans'>Blogs</h1>
+        </div>
+        <div className='flex flex-wrap justify-evenly mt-16'>
+             <div className='w-60 min-h-80 bg-gray-300 '>
+                <div>
+
+                </div>
+                 <div className='ś'>
+
+
+                 </div>
+             </div>
+             <div className='w-60 h-80 bg-gray-300'>
+
+             </div>
+             <div className='w-60 h-80 bg-gray-300'>
+
+                 <div className='w-60 h-80 bg-gray-300'>
+
+             </div>
+
+</div>
+        </div>
+    </div>
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default page
